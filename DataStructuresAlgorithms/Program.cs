bdf3fcafd2ea909bca4e0364a825c201503a71c2
Ashish Kumar Jha <jhaashish@outlook.in>
2020-09-03 10:21:00 +0530

    Add project files.

---
+++ DataStructuresAlgorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructuresAlgorithms
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Data STructures and Algorithm Repository.");
+        }
+    }
+}
